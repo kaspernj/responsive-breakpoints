53b0009eb204c16cc89c51b85c59fa31ba38636f
kaspernj <kasper@diestoeckels.de>
2026-04-21 05:21:59 +0000

    Revert "Merge pull request #12 from kaspernj/fix-default-breakpoint-types"
    This reverts commit 4ab88f6e4484f44ab06ad58780e5ae61187df439, reversing
    changes made to ed218c4a9fc05930974610d9d9883d3c545321ea.

--- src/use-breakpoint.js
+++ src/use-breakpoint.js
@@ -1,32 +1,7 @@
 // @ts-check
 import createUseBreakpoint from "./create-use-breakpoint.js"
 
-/**
- * @typedef {object} DefaultBreakpointState
- * @property {string} name Active breakpoint name.
- * @property {boolean} xsDown Whether viewport is at or below the xs breakpoint.
- * @property {boolean} xsUp Whether viewport is at or above the xs breakpoint.
- * @property {boolean} smDown Whether viewport is at or below the sm breakpoint.
- * @property {boolean} smUp Whether viewport is at or above the sm breakpoint.
- * @property {boolean} mdDown Whether viewport is at or below the md breakpoint.
- * @property {boolean} mdUp Whether viewport is at or above the md breakpoint.
- * @property {boolean} lgDown Whether viewport is at or below the lg breakpoint.
- * @property {boolean} lgUp Whether viewport is at or above the lg breakpoint.
- * @property {boolean} xlDown Whether viewport is at or below the xl breakpoint.
- * @property {boolean} xlUp Whether viewport is at or above the xl breakpoint.
- * @property {boolean} xxlDown Whether viewport is at or below the xxl breakpoint.
- * @property {boolean} xxlUp Whether viewport is at or above the xxl breakpoint.
- */
-
-/**
- * @typedef {{base?: object} & Record<string, object | undefined>} BreakpointStylingArgs
- */
-
-/**
- * @typedef {(args?: object) => {styling: (args: BreakpointStylingArgs) => object} & DefaultBreakpointState} DefaultUseBreakpointHook
- */
-
-/** @type {DefaultUseBreakpointHook} */
-const useBreakpoint = /** @type {DefaultUseBreakpointHook} */ (createUseBreakpoint())
+/** @type {ReturnType<typeof createUseBreakpoint>} */
+const useBreakpoint = createUseBreakpoint()
 
 export default useBreakpoint
